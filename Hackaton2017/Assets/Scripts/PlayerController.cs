8225c99047507f5941745fa1c2a4b6f729a0849a
Rémy Radix <remy.radix@gmail.com>
2017-10-28 19:59:25 +0200

    we have a player moving where the player clicks

--- Hackaton2017/Assets/Scripts/PlayerController.cs
+++ Hackaton2017/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+  public float WalkSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  private Vector3 ? _targetPosition;
+
+  // Use this for initialization
+  void Start() {
+    _targetPosition = null;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (Input.GetMouseButtonDown(0)) {
+      RaycastHit hit;
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      if (Physics.Raycast(ray, out hit, 100.0f)) {
+        Debug.Log("Clicked on " + hit.transform.name);
+
+        Debug.Log("Interraction not possible: moving...");
+        MoveTo(hit.point);
+      }
+    }
+
+    if (_targetPosition.HasValue) {
+      transform.position = Vector3.MoveTowards(transform.position, _targetPosition.Value, Time.deltaTime * WalkSpeed);
+      if ((_targetPosition.Value - transform.position).magnitude < .1f) {
+        transform.position = _targetPosition.Value;
+        _targetPosition = null;
+      }
+    }
+  }
+
+  public void OnCollisionEnter(Collision collision) {
+    _targetPosition = null;
+  }
+
+  private void PickItem() {
+
+  }
+
+  private void InteractWithItem() {
+
+  }
+
+  private void MoveTo(Vector3 target) {
+    // rotate to the point
+    Vector3 direction = target - transform.position;
+    direction.y = 0;
+    if (direction.magnitude > 1) {
+      direction.Normalize();
+    }
+    transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+
+    // move to the point
+    target.y = transform.position.y;
+    _targetPosition = target;
+  }
 }
