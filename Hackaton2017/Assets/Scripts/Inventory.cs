2700c51c410778d6a26cb482891a409ca1dc5874
Antoine Luciani <antoine1fr@gmail.com>
2017-10-28 16:52:17 +0200

    Implement inventory

---
+++ Hackaton2017/Assets/Scripts/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+	private PickableItem item;
+
+	// Use this for initialization
+	void Start () {
+		this.item = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	bool GrabItem(PickableItem item) {
+		if (!this.item)
+			return false;
+		this.item = item;
+		return true;
+	}
+
+	PickableItem Release() {
+		var item = this.item;
+		this.item = null;
+		return item;
+	}
+}
