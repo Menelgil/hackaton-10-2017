0f85b0f4492892b20f1d5754db93a9eb0921691f
Antoine Luciani <antoine1fr@gmail.com>
2017-10-29 12:53:28 +0100

    Implement RitualCaster behaviour

---
+++ Hackaton2017/Assets/Scripts/RitualCaster.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RitualCaster : MonoBehaviour {
+  public Ritual Ritual;
+
+	// Use this for initialization
+	private void Start () {
+	}
+
+	// Update is called once per frame
+	private void Update () {
+	}
+}
